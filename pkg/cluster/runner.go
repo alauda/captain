7d23b5d234df58bdd2090672eb2c383fb7b85c96
Hang Yan <hangyan@alauda.io>
2020-04-02 10:05:06 +0800

    Make captain more aware of cluster change
     1. faster refresher
     2. restart on cluster remove

--- pkg/cluster/runner.go
+++ pkg/cluster/runner.go
@@ -15,6 +15,9 @@ type ClusterRefresher struct {
 	ns  string
 }
 
+// NewClusterRefresher ...
+// This runnable intent to inform captain to restart when it found a new cluster. This is not a ideal solution, but it
+// works on most occasions. Add/Remove cluster should be a rear operation in prod environment.
 func NewClusterRefresher(ns string, cfg *rest.Config) *ClusterRefresher {
 	return &ClusterRefresher{
 		cfg: cfg,
@@ -39,14 +42,16 @@ func (c *ClusterRefresher) Start(stopCh <-chan struct{}) error {
 	}
 
 	for {
-		time.Sleep(30 * time.Second)
+		// short enough to avoid re-install kubernetes cluster on the same cluster.
+		time.Sleep(15 * time.Second)
 
 		latest, err := GetClusters(clusterClient, c.ns, opts)
 		if err != nil {
 			return err
 		}
 
-		if len(latest.Items) > len(origin.Items) {
+		// avoid re-install kubernetes cluster on the same nodes. Restart twice should work
+		if len(latest.Items) != len(origin.Items) {
 			klog.Info("possible new cluster added, restart captain")
 			time.Sleep(60 * time.Second)
 			os.Exit(0)
